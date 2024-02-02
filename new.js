cfb7b632073e5e261e726e316b322cb2f1f081a2
domlarsen7 <156616430+domlarsen7@users.noreply.github.com>
2024-02-02 11:48:19 -0600

    Updated

--- new.js
+++ new.js
@@ -1,8 +1,28 @@
 var arr = new Array();
 
-arr[0] = 9;
-arr[1] ='dom';
-arr[2]= true;
-arr[3]= yes;
+arr[0] = 1;
+arr[1] = 2;
+arr[2]= 3;
+arr[3]= 4;
 
-console.log(arr[1]);
+var funcExp = function addOne(number)
+ { console.log(arr[1]+1);}
+  
+ function oddOrEven(b,number) 
+ {
+    if
+    (b%2 == 0) 
+    {
+     number(b);
+    }
+      
+    else 
+    {
+        console.log("the number is odd");
+    }
+ }    
+
+ for (f in arr)
+ {
+    oddOrEven(arr[f], funcExp);
+ }
