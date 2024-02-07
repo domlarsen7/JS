e88ec9d8a1cefa260d464178a59f233030e10e9f
domlarsen7 <156616430+domlarsen7@users.noreply.github.com>
2024-02-07 16:52:18 -0600

    Save

--- dom4.js
+++ dom4.js
@@ -3,11 +3,14 @@ console.log(teamName);
 
 const par = document.createElement("p");
 
+const tms = ["Vikings", "Packers", "Chiefs", "Broncos", "Cowboys"];
 
-for (let msg of teamName)
+for (let tm of tms)
 {
     const p = document.createElement("p");
-    p.teamName.add("Broncos");
-    const text = document.createTextNode(msg);
+    const text = document.createTextNode(tm);
     p.appendChild(text);
+    div.appendChild(p);
 }
+const node = $("div");
+
